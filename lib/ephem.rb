f61e07f7079700fd12e5323dac749668d218c88c
Rémy Hannequin <remy@thoughtbot.com>
2025-01-01 22:59:16 +0100

    Build gem

---
+++ lib/ephem.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "ephem/version"
+
+module Ephem
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/ephem_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe Ephem do
+  it "has a version number" do
+    expect(Ephem::VERSION).not_to be nil
+  end
+end
