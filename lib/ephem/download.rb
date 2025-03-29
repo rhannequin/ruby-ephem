f5eba9303c9d9777754e5570d6faccefced607ec
Rémy Hannequin <remy@thoughtbot.com>
2025-03-29 23:58:48 +0100

    IMCCE INPOP support
    I can't hide this change makes me super happy.
    
    _INPOP_ is the ephemerides series from the Paris Observatory, more
    precisely from the IMCCE laboratory. The Paris Observatory has been a
    central place for astronomy for the last few centuries and the data
    produces today by the IMCCE is among the most accurate and precise we
    can produce today.
    
    I frequently use data from the IMCCE in the development and tests of
    Astronoby, main library using Ephem at the moment, so it feels right to
    support their ephemerides as well.
    
    I am extremely lucky that they offer a SPICE version of their ephemeris
    files for most of INPOP's versions. As you can see, this change doesn't
    implement any modification of the SPK parsing logic, it only adds the
    support for downloading files from somewhere else than the JPL.
    
    The choice between JPL and IMCCE is just a matter of preference, their
    data being extremely close. While you can witness differences of a few
    kilometers in positions, the difference becomes barely noticeable when
    used in libraries transforming the data into topocentric coordinates
    like Astronoby.
    
    The list of supported files is:
    * `inpop10b.bsp`
    * `inpop10b_large.bsp`
    * `inpop10e.bsp`
    * `inpop10e_large.bsp`
    * `inpop13c.bsp`
    * `inpop13c_large.bsp`
    * `inpop17a.bsp`
    * `inpop17a_large.bsp`
    * `inpop19a.bsp`
    * `inpop19a_large.bsp`
    * `inpop21a.bsp`
    * `inpop21a_large`
    
    The difference between ephemerides with or without `_large` is the time
    period: from 1900 to 2100 for regular ephemerides, from 1000 to 3000,
    Common Era.

--- lib/ephem/download.rb
+++ lib/ephem/download.rb
@@ -1,12 +1,16 @@
 # frozen_string_literal: true
 
+require "minitar"
 require "net/http"
+require "tempfile"
+require "zlib"
 
 module Ephem
   class Download
-    BASE_URL = "https://ssd.jpl.nasa.gov/ftp/eph/planets/bsp/"
+    JPL_BASE_URL = "https://ssd.jpl.nasa.gov/ftp/eph/planets/bsp/"
+    IMCCE_BASE_URL = "https://ftp.imcce.fr/pub/ephem/planets/"
 
-    SUPPORTED_KERNELS = %w[
+    JPL_KERNELS = %w[
       de102.bsp
       de200.bsp
       de202.bsp
@@ -53,6 +57,39 @@ module Ephem
       de441.bsp
     ].freeze
 
+    IMCCE_KERNELS = {
+      "inpop10b.bsp" => "inpop10b_TDB_m100_p100_spice.bsp",
+      "inpop10b_large.bsp" => "inpop10b_TDB_m1000_p1000_spice.bsp",
+      "inpop10e.bsp" => "inpop10e_TDB_m100_p100_spice.bsp",
+      "inpop10e_large.bsp" => "inpop10e_TDB_m1000_p1000_spice.bsp",
+      "inpop13c.bsp" => "inpop13c_TDB_m100_p100_spice.bsp",
+      "inpop13c_large.bsp" => "inpop13c_TDB_m1000_p1000_spice.bsp",
+      "inpop17a.bsp" => "inpop17a_TDB_m100_p100_spice.bsp",
+      "inpop17a_large.bsp" => "inpop17a_TDB_m1000_p1000_spice.bsp",
+      "inpop19a.bsp" => "inpop19a_TDB_m100_p100_spice.bsp",
+      "inpop19a_large.bsp" => "inpop19a_TDB_m1000_p1000_spice.bsp",
+      "inpop21a.bsp" => "inpop21a_TDB_m100_p100_spice.bsp",
+      "inpop21a_large.bsp" => "inpop21a_TDB_m1000_p1000_spice.bsp"
+    }.freeze
+
+    IMCCE_KERNELS_MATCHING = {
+      "inpop10b.bsp" => "inpop10b/inpop10b_TDB_m100_p100_spice.tar.gz",
+      "inpop10b_large.bsp" => "inpop10b/inpop10b_TDB_m1000_p1000_spice.tar.gz",
+      "inpop10e.bsp" => "inpop10e/inpop10e_TDB_m100_p100_spice_release2.tar.gz",
+      "inpop10e_large.bsp" =>
+        "inpop10e/inpop10e_TDB_m1000_p1000_spice_release2.tar.gz",
+      "inpop13c.bsp" => "inpop13c/inpop13c_TDB_m100_p100_spice.tar.gz",
+      "inpop13c_large.bsp" => "inpop13c/inpop13c_TDB_m1000_p1000_spice.tar.gz",
+      "inpop17a.bsp" => "inpop17a/inpop17a_TDB_m100_p100_spice.tar.gz",
+      "inpop17a_large.bsp" => "inpop17a/inpop17a_TDB_m1000_p1000_spice.tar.gz",
+      "inpop19a.bsp" => "inpop19a/inpop19a_TDB_m100_p100_spice.tar.gz",
+      "inpop19a_large.bsp" => "inpop19a/inpop19a_TDB_m1000_p1000_spice.tar.gz",
+      "inpop21a.bsp" => "inpop21a/inpop21a_TDB_m100_p100_spice.tar.gz",
+      "inpop21a_large.bsp" => "inpop21a/inpop21a_TDB_m1000_p1000_spice.tar.gz"
+    }.freeze
+
+    SUPPORTED_KERNELS = (JPL_KERNELS + IMCCE_KERNELS.keys).freeze
+
     def self.call(name:, target:)
       new(name, target).call
     end
@@ -64,8 +101,7 @@ module Ephem
     end
 
     def call
-      uri = URI("#{BASE_URL}#{@name}")
-      content = Net::HTTP.get(uri)
+      content = jpl_kernel? ? download_from_jpl : download_from_imcce
       File.write(@local_path, content)
 
       true
@@ -78,6 +114,34 @@ module Ephem
         raise UnsupportedError,
           "Kernel #{@name} is not supported by the library at the moment."
       end
+    end
+
+    def jpl_kernel?
+      JPL_KERNELS.include?(@name)
+    end
+
+    def download_from_jpl
+      uri = URI("#{JPL_BASE_URL}#{@name}")
+      Net::HTTP.get(uri)
+    end
+
+    def download_from_imcce
+      temp_file = Tempfile.new(%w[archive .tar.gz])
+      uri = URI("#{IMCCE_BASE_URL}#{IMCCE_KERNELS_MATCHING[@name]}")
+      content = Net::HTTP.get(uri)
+      temp_file.write(content)
+      temp_file.rewind
+
+      Zlib::GzipReader.open(temp_file.path) do |gz|
+        Minitar::Reader.open(gz) do |tar|
+          tar.each_entry do |entry|
+            return entry.read if entry.full_name == IMCCE_KERNELS[@name]
+          end
+        end
+      end
+    ensure
+      temp_file.close
+      temp_file.unlink
     end
   end
 end
